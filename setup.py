c11d62171f246b14c5c0cd348aaff515ced91e63
Xolani Silangwe <Xolani@explore-datascience.net>
2019-03-19 15:58:23 +0200

    updating project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
+    name='xolani',
     version='0.1.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
